25f951a07dd374ce73074eb822bbb2761ef05365
Colin Rundel <rundel@gmail.com>
2014-10-27 21:21:15 -0400

    Clean up, throw error on test failure

--- hw4/tests/test_pagerank.R
+++ hw4/tests/test_pagerank.R
@@ -2,7 +2,17 @@ library(testthat)
 
 context("Test Adjacency Matrix")
 
-test_that("Creation", {
+test_that("Addition", {
     expect_that(1+1, equals(2))
+    expect_that(1+2, equals(2))
     expect_that(1+3, equals(2))
+    expect_that(1+4, equals(5))
 })
+
+test_that("Subtraction", {
+    expect_that(1-1, equals(0))
+    expect_that(2-1, equals(2))
+    expect_that(3-1, equals(2))
+    expect_that(4-1, equals(3))
+})
+
