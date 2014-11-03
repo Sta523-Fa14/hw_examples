0c4dd1bcad961520bc165f8997d285ba30c41249
Colin Rundel <rundel@gmail.com>
2014-11-02 22:42:47 -0500

    Add file io tests

--- hw4/file_io.R
+++ hw4/file_io.R
@@ -1,6 +1,7 @@
 write_graph = function(g, file)
 {
     stopifnot(!file.exists(file))
+    stopifnot(file.exists(dirname(file)))
 
     write("",file=file)
 }
@@ -8,7 +9,7 @@ write_graph = function(g, file)
 
 read_graph = function(file)
 {
-    stopifnot(!file.exists(file))
+    stopifnot(file.exists(file))
 
     return(list(list()))
 }
